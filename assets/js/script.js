08180cac329aa1baa0496404677402cc0aff3726
Rafael <faelsant10@gmail.com>
2021-08-23 19:49:46 -0300

    projeto curso html

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+let data = [];
+
+function renderTodo() {
+
+  document.querySelector('.todo').innerHTML = '';
+
+  data.forEach(task => {
+    let li = document.createElement('li');
+
+    li.innerHTML = `
+    <input type="checkbox" id="task-${task.id}">
+    <label for="task-${task.id}">${task.title}</label>
+    <button>X</button>`;
+
+    li.querySelector('input').addEventListener("change", e => {
+      if (e.target.checked) {
+        li.classList.add('complite');
+      } else {
+        li.classList.remove('complite');
+      }
+    });
+
+    li.querySelector('button').addEventListener('click',
+      e => {
+        let button = e.target;
+        let li = button.parentNode;
+        let input = li.querySelector('input');
+        let id = input.id;
+        let idArray = id.split('-');
+        let todoId = idArray[1];
+
+
+
+        data = data.filter(task => task.id !== parseInt(todoId));
+
+        renderTodo();
+
+      });
+
+
+
+    document.querySelector('.todo').append(li);
+  });
+
+}
+
+document.querySelector('#new-task').addEventListener('keyup', e => {
+  if (e.key === 'Enter') {
+
+
+    data.push({
+      id: data.length+1,
+      title: e.target.value
+    });
+
+    e.target.value = "";
+
+    renderTodo();
+  }
+});
+
+renderTodo();
